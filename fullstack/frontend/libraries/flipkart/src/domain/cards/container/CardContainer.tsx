a6f9c737a8e07bb6d151bce0f788d51baf207857
wa5 <waseemslyar@gmail.com>
2022-02-20 07:24:02 +0530

    dfghfdghj

--- fullstack/frontend/libraries/flipkart/src/domain/cards/container/CardContainer.tsx
+++ fullstack/frontend/libraries/flipkart/src/domain/cards/container/CardContainer.tsx
@@ -8,24 +8,30 @@ interface IStateProps{
 }
 const mapStateToprops=(state: any)=>{
     console.log("ghh",state?.todo?.todos[0]?.title)
+    
     return {
         name:state?.todo?.todos[0]?.title
     }
 }
-
-const MyMediaCardViews:React.FC<IStateProps>=props=>{
-    const dispach =useDispatch()
-    useEffect(()=>{
-        dispach(fetchTodoRequest())
-
-        },[])
-    return (
-<MyMediaCard name={props.name}/>
-    )
+const mapDispatchToprops=(dispatch: any)=>{
+    dispatch(fetchTodoRequest())
+    
+    return {}
+   
 }
+// const MyMediaCardViews:React.FC<IStateProps>=props=>{
+//     const dispach =useDispatch()
+//     useEffect(()=>{
+//         dispach(fetchTodoRequest())
+
+//         },[])
+//     return (
+// <MyMediaCard name={props.name}/>
+//     )
+// }
    
 
-export const  CardContainer:React.ComponentType=connect<IStateProps>(mapStateToprops)(MyMediaCardViews)
+export const  CardContainer:React.ComponentType=connect<IStateProps>(mapStateToprops,mapDispatchToprops)(MyMediaCard)
 
 
 
