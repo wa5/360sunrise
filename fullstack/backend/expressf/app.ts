0597bf3cd01b9ce49ae19cec77b567041177a8f1
wa5 <waseemslyar@gmail.com>
2022-03-02 20:50:51 +0530

    sdfghjkl

--- fullstack/backend/expressf/app.ts
+++ fullstack/backend/expressf/app.ts
@@ -1,19 +1,90 @@
 import express from "express";
+require('dotenv').config()
 const app = express();
+app.set('port',process.env.PORT||3001)
 
-app.get("/", (req, res) => {
-  res.send("<h1>this is my first api get</h1>");
-});
-app.post("/", (req, res) => {
-  res.send("this is my first api post");
-});
-app.put("/", (req, res) => {
-  res.send("this is my first api put");
-});
-app.delete("/", (req, res) => {
-  res.send("this is my first api delte");
-});
+const path=require('path')
+const ejs=require('ejs')
+app.set('view engine','ejs')
+app.set('views',path.join(__dirname,'src/views'))
 
-app.listen(3000, () => {
-  console.log("server is started ");
+
+
+
+app.get('/home',(req,res)=>{
+    var g=5666
+    res.render('home',{key:g})
+})
+app.get('/about',(req,res)=>{
+    var g=5666
+    res.render('about')
+})
+
+
+
+
+
+
+//url
+// app.get('/home/:num',(req,res)=>{
+//     res.send(`im a home api${req.params.num}`)
+//     })
+
+//8 for anytheng in between
+// app.get('/ho*me',(req,res)=>{
+//     res.send("im a home api")
+//     })
+
+//+1 or more charecters like it
+// app.get('/h+o+m+e+',(req,res)=>{
+//     res.send("im a home api")
+//     })
+
+//? 0 or more times u can give ur charecters
+// app.get('/h?o?m?e?',(req,res)=>{
+// res.send("im a home api")
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.route("/")
+// .get((req, res) => {
+//   res.send("<h1>this is my first api get</h1>");
+// }).post( (req, res) => {
+//   res.send("this is my first api post");
+// }).put( (req, res) => {
+//   res.send("this is my first api put");
+// }).delete( (req, res) => {
+//   res.send("this is my first api delte");
+// })
+
+app.listen(app.get('port'), () => {
+  console.log(`server is started:${app.get('port')} `);
 });
